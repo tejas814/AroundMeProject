7772310aeb57bb394c5422277d491872f1cc3e6d
Itsantg <tejasgaikwad814@gmail.com>
2024-10-18 19:17:46 +0530

    addProject

---
+++ src/main/java/com/nt/AroundMeStarter.java
@@ -0,0 +1,12 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AroundMeStarter {
+
+	public static void main( String[] args ) {
+		SpringApplication.run( AroundMeStarter.class, args );
+	}
+}
